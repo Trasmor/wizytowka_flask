aff4b26606b35f338272a92d4750b74793f5eca3
Trasmor <106475434+Trasmor@users.noreply.github.com>
2022-10-20 09:39:48 +0200

    Add files via upload

---
+++ wizytowka_flask.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/mypage/me')
+def mypage():
+    return render_template('me.html')
+
+@app.route('/mypage/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template('contact.html')
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"  
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
